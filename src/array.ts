7a7be07a1a96a3fd0644e4eee568a84d518138a3
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-02 16:32:04 +0600

    array_touple

--- src/array.ts
+++ src/array.ts
@@ -1,3 +1,30 @@
-const namess = ["abul ", "kabul", "babul"] ;
+const namess: string[] = ["abul ", "kabul", "babul"] ;
 // or 
-// const namess : string = ["abul ", "kabul", "babul"] ;
+// const namess : string[] = ["abul ", "kabul", "babul"] ;
+
+const rollNumbers : number[] = [23,45,234,56,33]
+
+// rollNumbers.map(roll=>roll.);
+// namess.map(name=>name.)
+
+
+// dhori ami array er moddhe shob kicu rakhte cai i mean string number , bollean EventCounts
+const villageName =[ "jourpukuria",23,true]
+
+
+
+// ekhon ami touple shikbo ; touple couple er moto joray joray 
+const user = [ "jahidul ", "Rahim ", "amin ", "kamin"];
+// etate ami posh , pop aro onk kicu change korte parbo 
+// ebong 
+// jemon 
+// user[2]= 34;
+
+
+// but touple ta hobe emon ; 
+
+
+const users: [string, number] = ["jahidul ", 123]; 
+// ei rokom kore define korte parbo 
+
+// ekhon ami caileo 0 number index e number assign korte parbona 
