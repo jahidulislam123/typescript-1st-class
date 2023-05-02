4bc9161f8859ef97172255dce9ae967c6b520145
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-02 16:46:23 +0600

    Object, Literal Types and Optional types

---
+++ src/object.ts
@@ -0,0 +1,44 @@
+const userss : {
+    name:string,
+    age:number,
+    isMarried:boolean,
+    wife ?:string
+} ={
+    name : "Jahidul Islam",
+    age : 52 , 
+    isMarried : true,
+    wife : 'BIBI'
+};
+
+// specific string 
+const usersss : {
+    company : "programming hero "
+    name:string,
+    age:number,
+    isMarried:boolean,
+    wife ?:string
+} ={
+    //jodi emon hoy ami company er nam specific string dibo change hote parbena 
+    company: "programming hero ",
+    name : "Jahidul Islam",
+    age : 52 , 
+    isMarried : true,
+    wife : 'BIBI'
+};
+
+
+// readonly string 
+const userssss : {
+    readonly company : string,
+    name:string,
+    age:number,
+    isMarried:boolean,
+    wife ?:string
+} ={
+    //jodi emon hoy ami company er nam specific string dibo change hote parbena 
+    company: "programming hero ",
+    name : "Jahidul Islam",
+    age : 52 , 
+    isMarried : true,
+    wife : 'BIBI'
+};
