834a1786585faaf93cf5445bb7d49d78e9ad3ec8
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-02 16:16:19 +0600

    premitivi_typescript

--- src/primitive.ts
+++ src/primitive.ts
@@ -6,10 +6,31 @@
 // undefined 
 //jemon 
 
-const courses : string = " Next Level Web developement ";
-const rollNumber : number = 123;
+let courses : string = " Next Level Web developement ";
+let rollNumber : number = 123;
 const isAdmin : boolean = true ; 
 
 // ami jodi ekhane 
 courses = "next level developemnet " ; 
 // tahole hobe na karon eta type string hoye ace age 
+
+
+// ami roll number tao change korete parbo 
+rollNumber = 123 ;
+
+// ekhn ami caile type gula define na kore dileo hobe auto dhore nibe 
+const names = " jahidul islam ";
+const id = 455;
+
+
+
+// jodi kono value na dei type o na dei tahole porobbortitie jekhono kicu use korte pari
+let dool ;
+
+
+// but emon dile hobe na 
+let mona : string;
+// mona =34;
+
+
+let minp : undefined;
