76b0bba83600277bcc69e082ad30450e99d86920
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-02 16:16:00 +0600

    premitivi_typescript

---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+// ekhane shob primitive types somporke janbo 
+// string 
+// number 
+// boolean 
+//  null
+// undefined 
+//jemon 
+
+const courses : string = " Next Level Web developement ";
+const rollNumber : number = 123;
+const isAdmin : boolean = true ; 
+
+// ami jodi ekhane 
+courses = "next level developemnet " ; 
+// tahole hobe na karon eta type string hoye ace age 
