4f1e3a486bb3d68b742a654041fd45ea0db68352
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-04 11:12:19 +0600

    Introduction of Generic in Type

---
+++ src/module_3/genetic_types.ts
@@ -0,0 +1,92 @@
+type GenericArray = Array<number>
+const rrollNumberd: GenericArray=[44,23,45];
+const rollNumbe22e:GenericArray=["44","23","65"];
+// ekhon jodi string pathay tahole tw kaj korbe na error dekhabe . ekhon ki kora jay 
+//  tar jonno ekta boddi aci . generic er angle e ekta T ba onno kono name peramiter diye dibo jate 
+// kore jekono type recevive korte pre 
+
+
+type GenericArrayy<T> = Array<T>
+const rrollNumberdd: GenericArrayy<number>=[44,23,45];
+const rollNumbe22ed:GenericArrayy<string>=["44","23","65"];
+const rollBolleann : GenericArrayy<boolean > =[true,false]
+const userNameAndRollNumberr:GenericArrayy<{name:string,roll:number}>=[{
+    name:"mr. x",
+    roll:1
+},{
+    name:"mr.y",
+    roll:3
+}
+] 
+type NameObject ={
+    name:string,
+        roll:number
+}
+const userNameAndRollNumberrr:GenericArrayy<NameObject>=[{
+    name:"mr. x",
+    roll:1
+},{
+    name:"mr.y",
+    roll:3
+}
+] 
+
+// ekhane object er name and object takeo type banate pari 
+
+// ei je erokon kore likle hobe 
+// r etai generic type 
+
+
+
+
+// generic mane generalize 
+const rrollNumber: number[]=[44,23,45];
+// etake evabeo likha jay
+const rollNumbersss: Array<number> =[44,23,45]
+const rollNumbe22:string[]=["44","23","65"];
+// same etakeo emon kora jay 
+const rollNumbe222 :Array<string> =["44","23","65"]
+
+
+// bollean er jonno ja korbo 
+const rollBollean : Array<boolean > =[true,false]
+
+const userNameAndRollNumber:Array<{name:string,roll:number}>=[{
+    name:"mr. x",
+    roll:1
+},{
+    name:"mr.y",
+    roll:3
+}
+] 
+
+
+
+
+// amra caile eker odhik o generic type er argument nite pari 
+// jemon
+
+type GenericToupole <X,Y> = [X,Y]
+const relation:GenericToupole <string,string>=["jahid","jahid"] ;
+
+const relationonWithSalary : GenericToupole<object,string>=[{
+    name:'persian',
+    salary : 10 
+},
+"jahidul"
+]
+
+
+
+// emon jodi hoy object er vitore amake select kore dibo name thakbe ar roll thakbe specificly bole dite hobe 
+
+type GenericToupoles <X,Y> = [X,Y]
+const relations:GenericToupoles <string,string>=["jahid","jahid"] ;
+
+const relationonWithSalarys : GenericToupole<{name:string,salary:number},string>=[{
+    name:'persian',
+    salary : 10 
+},
+"jahidul"
+]
+
