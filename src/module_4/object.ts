d2828852f6b26fb107ba7b9e219651464aa8a4c6
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-07 19:46:13 +0600

    How to Create Class, Object , Parameter Properties

--- src/module_4/object.ts
+++ src/module_4/object.ts
@@ -1,10 +1,15 @@
 // class create korbo 
 class Animal {
     // animal class er moddhe ei ei properties gula thakbe 
-    name:string;
-    species:string;
-    sound : string;
+
+
+    // by default vabe public thake ami caile samne public likhteo pari nao likhte pari 
+
+   public name:string;
+   public species:string;
+   public sound : string;
     // class likhle evabe constructor declare korte hoy
+    // opore properties likhlam abar constructor e o likhlam eke paremater properties  bole 
     constructor(name:string, species:string, sound:string){
         this.name=name;
         this.species=species;
@@ -13,10 +18,17 @@ class Animal {
 
     // Class e kono function likhle eke bola hoy method 
     // But arrow function use korbo na normal function use korbo 
-    makesound(){
+    public makesound(){
         console.log(`th ${this.name} says ${this.sound}`);
     }
 }
 
 const dog = new Animal('german shepard','dog','ghe2w ghew')
 dog.makesound();
+
+const cat =new Animal("pat","dammay","lalu");
+cat.makesound();
+
+
+// ekhon ami cat er nam change korte parbo 
+cat.name="pappai"
