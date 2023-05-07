9c8ee97c658191678621cc6b59a11e2b4abea259
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-07 22:54:06 +0600

    Access Modifiers: Public, Private, Protected

--- src/module_4/access_modifire.ts
+++ src/module_4/access_modifire.ts
@@ -2,15 +2,28 @@
 class BankAccount {
     id : number;
     name : string;
-    balance : number;
-    constructor(id:number,name:string,balance:number){
-        this.balance=balance;
+    private _balance : number;
+    // private dile only claass er vitore use kora lage
+    constructor(id:number,name:string,_balance:number){
+        this._balance=_balance;
         this.id=id;
         this.name=name;
 
     }
+    gerBalance(){
+        console.log(`My balance is ${this._balance}`)// sudho class er vitorei access  korte parbo 
+
+    }
+    deposite(amount :number){
+        this._balance = this._balance+amount;
+    }
 }
 
 
+
+
 const myAccount = new BankAccount(444,'persian',23);
+// myAccount.balance=0;
+// manush evabe balance change kroe dite pare 
+// tai ami private kore dibo 
 console.log(myAccount);
