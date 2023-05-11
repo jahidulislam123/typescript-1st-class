9e30ce5b5bc853f43269666da056f8156630e2dc
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-11 16:43:36 +0600

    added

--- src/module5_5/main.ts
+++ src/module5_5/main.ts
@@ -1,10 +1,61 @@
-import { add as addtwo ,substruct} from "./module";
+// import { add as addtwo ,substruct,multiplu} from "./module";
+// amra evabe protidin comma diye na likhe onno vabe korte pari 
+// import * as Method from "./module"
+// // default ta jevabe import korbo ;
+
+// import multiplu  from "./module";
+// const add = (param1:number,param2:number,param3:number):number=>{
+//     return param1+param2+param3;
+// }
+
+// // export import er maddome share korte parbo 
+
+// // const res = addtwo(4,5);
+
+// // const res2 =substruct(5,4)
+
+// // const res3 = multiplu(4,5);
+
+
+
+// // ekhano evabe korte hobe 
+// const res = Method.add(4,5);
+
+// const res2 =Method.substruct(5,4)
+
+// const res3 = multiplu(4,5);
+
+
+
+
+
+
+
+
+
+
+
+// finally ami jevabe korbo 
+// import { add as addtwo ,substruct,multiplu} from "./module";
+// amra evabe protidin comma diye na likhe onno vabe korte par
+import jekono  from "./module";
 const add = (param1:number,param2:number,param3:number):number=>{
     return param1+param2+param3;
 }
 
 // export import er maddome share korte parbo 
 
-const res = addtwo(4,5);
+// const res = addtwo(4,5);
 
-const res2 =substruct(5,4)
+// const res2 =substruct(5,4)
+
+// const res3 = multiplu(4,5);
+
+
+
+// ekhano evabe korte hobe 
+const res = jekono.add(4,5);
+
+const res2 =jekono.multiplu(5,4)
+
+const res3 = jekono.substruct(4,5);
--- src/module5_5/module.ts
+++ src/module5_5/module.ts
@@ -1,8 +1,43 @@
-export const add = (param1:number,param2:number):number=>{
+// export const add = (param1:number,param2:number):number=>{
+//     return param1+param2;
+// }
+
+
+// export const substruct=(param1:number,param2:number):number=>{
+//     return param1+param2;
+// }
+// const multiplu=(param1:number,param2:number):number=>{
+//     return param1*param2;
+// }
+
+
+// // amra caile default add korte pari 
+
+// export default multiplu;
+
+
+
+
+// finally ami jevabe korbo :
+ const add = (param1:number,param2:number):number=>{
     return param1+param2;
 }
 
 
-export const substruct=(param1:number,param2:number):number=>{
+ const substruct=(param1:number,param2:number):number=>{
     return param1+param2;
 }
+const multiplu=(param1:number,param2:number):number=>{
+    return param1*param2;
+}
+
+
+// amra caile default add korte pari 
+
+export default {
+    add
+    ,
+    multiplu,
+    substruct,
+
+};
