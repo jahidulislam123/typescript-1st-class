073a3487f27a368839e278adee3adbdb9e6fa0e5
jahidulislam123 <islam15-3879@diu.edu.bd>
2023-05-11 16:20:32 +0600

    added

---
+++ src/module5_5/main.ts
@@ -0,0 +1,10 @@
+import { add as addtwo ,substruct} from "./module";
+const add = (param1:number,param2:number,param3:number):number=>{
+    return param1+param2+param3;
+}
+
+// export import er maddome share korte parbo 
+
+const res = addtwo(4,5);
+
+const res2 =substruct(5,4)
---
+++ src/module5_5/module.ts
@@ -0,0 +1,8 @@
+export const add = (param1:number,param2:number):number=>{
+    return param1+param2;
+}
+
+
+export const substruct=(param1:number,param2:number):number=>{
+    return param1+param2;
+}
